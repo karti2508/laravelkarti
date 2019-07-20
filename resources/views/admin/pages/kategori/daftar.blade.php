e97ff4ef18dd3329bf009784259e828ace29e514
Karti <kartikalisna2608@gmail.com>
2019-07-20 10:36:26 +0700

    Video Eps28-29

--- resources/views/admin/pages/kategori/daftar.blade.php
+++ resources/views/admin/pages/kategori/daftar.blade.php
@@ -22,9 +22,27 @@
 </div>
 @endif
 
+@if( session('result') == 'delete' )
+<div class="alert alert-success alert-dismissible fade show">
+	<strong>Deleted !</strong> Berhasil dihapus.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
+@if( session('result') == 'fail-delete' )
+<div class="alert alert-success alert-dismissible fade show">
+	<strong>Failed !</strong> Gagal Dihapus.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
 <div class="row">
 	<div class="col-md-6 mb-3">
-		<a href="{{ route('admin.kategori.add') }}" class="btn btn-primary">[+] Tambah</a>
+		<a href="{{route('admin.kategori.add') }}" class="btn btn-primary">[+] Tambah</a>
 	</div>
 
 	<div class="col-md-6 mb-3">
@@ -54,9 +72,12 @@
 			class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
-			<button type="button" class="btn btn-danger btn-sm">
+			<button type="button" 
+			data-id="{{ $dt->id }}"
+			class="btn btn-danger btn-sm btn-trash">
 				<i class="fa fa-w fa-trash"></i>
 			</button>
+
 		</td>
 	</tr>
 	@endforeach
@@ -68,3 +89,50 @@
 }}
 
 @endsection
+
+@push('modal')
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			
+			<div class="modal-header">
+				<h5 class="modal-title">Delete</h5>
+				<button class="close" type="button" data-dissmis="modal">
+					<span aria-hidden="true">x</span>
+				</button>
+			</div><!--End Modal Header-->
+
+			<div class="modal-body">
+				Apakah anda yakin ingin menghapusnya?
+				<form id="form-delete" method="post" action="{{ route('admin.kategori') }}">
+					{{ method_field('delete') }}
+					{{ csrf_field()}}
+					<input type="hidden" name="id" id="input-id">
+				</form>
+			</div><!--End Modal Body-->
+
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+				<button class="btn btn-primary btn-delete" type="button">Delete</button>
+			</div><!--End Modal footer-->
+
+		</div>
+	</div>
+</div>
+@endpush
+
+@push('js')
+<script type="text/javascript">
+	$(function(){
+		$('.btn-trash').click(function(){
+			id = $(this).attr('data-id');
+			$('#input-id').val(id);
+			$('#deleteModal').modal('show');
+		});
+
+		$('.btn-delete').click(function(){
+			$('#form-delete').submit();
+		});
+	});
+</script>
+@endpush
