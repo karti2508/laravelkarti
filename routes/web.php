a88b8cd786e623af379a537b9c49eb586544007c
Karti <kartikalisna2608@gmail.com>
2019-07-20 14:15:09 +0700

    Video Eps30-32

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,10 @@ Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
 		Route::post('/edit/{id}','KategoriController@update');
 	});
 
+	/* Produk */
+	Route::group(['prefix'=>'produk','middleware'=>'akses.admin'], function(){
+		Route::get('/','ProdukController@daftar')->name('admin.produk');
+	});
 });
 
 
